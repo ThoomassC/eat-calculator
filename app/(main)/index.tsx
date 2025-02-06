f232e73b64377c91939926bc5be8b147c561f8b7
Thomas <thomas.caron@linkt.fr>
2025-02-06 11:44:03 +0100

    feat : css index

--- app/(main)/index.tsx
+++ app/(main)/index.tsx
@@ -17,20 +17,34 @@ const HomeScreen = () => {
   const router = useRouter();
 
   const renderItem = ({ item }: { item: any }) => (
-    <TouchableOpacity onPress={() => router.push(`/${item.id}`)}>
+    <TouchableOpacity
+      onPress={() => router.push(`/${item.id}`)}
+      style={styles.mealItemContainer}
+    >
       <View style={styles.mealItem}>
-        {item.image && (
-          <Image source={{ uri: item.image }} style={styles.mealImage} />
-        )}
         <View style={styles.mealInfo}>
-          <Text style={styles.mealName}>{item.name}</Text>
+          <Text style={styles.mealName} numberOfLines={2} ellipsizeMode="tail">
+            {item.name}
+          </Text>
           <Text style={styles.mealCalories}>{item.calories} kcal</Text>
           <FlatList
             data={item.items}
             renderItem={({ item }) => (
               <View style={styles.foodItem}>
-                <Image source={{ uri: item.image }} style={styles.foodImage} />
-                <Text>
+                {item.image ? (
+                  <Image
+                    source={{ uri: item.image }}
+                    style={styles.foodImage}
+                  />
+                ) : (
+                  <Ionicons
+                    name="image-outline"
+                    size={30}
+                    color="#ccc"
+                    style={styles.defaultFoodImage}
+                  />
+                )}
+                <Text style={styles.foodText}>
                   {item.name} x {item.quantity} -{" "}
                   {item.calories * item.quantity} kcal
                 </Text>
@@ -39,6 +53,9 @@ const HomeScreen = () => {
             keyExtractor={(item) => item.id.toString()}
           />
         </View>
+        {item.image && (
+          <Image source={{ uri: item.image }} style={styles.mealImage} />
+        )}
       </View>
     </TouchableOpacity>
   );
@@ -49,6 +66,7 @@ const HomeScreen = () => {
         data={meals}
         renderItem={renderItem}
         keyExtractor={(item) => item.id.toString()}
+        contentContainerStyle={styles.listContent}
       />
       <TouchableOpacity
         style={styles.addButton}
@@ -65,52 +83,88 @@ export default HomeScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: "#f5f5f5",
+    padding: 16,
+  },
+  listContent: {
+    paddingBottom: 80,
+  },
+  mealItemContainer: {
+    marginTop: 10,
+    borderRadius: 8,
     backgroundColor: "#fff",
-    padding: 24,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 8,
+    elevation: 2,
+    borderWidth: 1,
+    borderColor: "#000",
+    marginLeft: 5,
+    marginRight: 5,
   },
   mealItem: {
     flexDirection: "row",
+    padding: 8,
+    borderRadius: 8,
     justifyContent: "space-between",
-    padding: 16,
-    borderBottomWidth: 1,
-    borderBottomColor: "#ccc",
   },
   mealImage: {
     width: 50,
     height: 50,
-    marginRight: 16,
+    borderRadius: 8,
   },
   mealInfo: {
     flex: 1,
     justifyContent: "center",
+    marginRight: 16,
   },
   mealName: {
-    fontSize: 18,
+    fontSize: 16,
+    fontWeight: "bold",
   },
   mealCalories: {
-    fontSize: 18,
+    fontSize: 14,
     color: "#888",
+    marginTop: 4,
   },
   foodItem: {
     flexDirection: "row",
     alignItems: "center",
-    padding: 10,
-    borderBottomColor: "#ccc",
+    marginTop: 8,
   },
   foodImage: {
     width: 30,
     height: 30,
     marginRight: 10,
+    borderRadius: 8,
+  },
+  defaultFoodImage: {
+    width: 30,
+    height: 30,
+    marginRight: 10,
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  foodText: {
+    fontSize: 14,
+    color: "#555",
   },
   addButton: {
     position: "absolute",
     bottom: 20,
     right: 20,
-    backgroundColor: "#2196F3",
+    backgroundColor: "#4CAF50",
     width: 60,
     height: 60,
     borderRadius: 30,
     alignItems: "center",
     justifyContent: "center",
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 4,
+    elevation: 5,
   },
 });
